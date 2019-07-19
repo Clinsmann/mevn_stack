1df867a9ceb3097b087f97d7bf9d32354b70f930
Hillary <Ibeanuhillary@gmail.com>
2019-07-19 17:08:07 +0100

    production database added

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,11 @@
-module.exports = {
-    //mongoURI:"mongodb://localhost/mevn_stack",
-    mongoURI:"mongodb+srv://clinsmann:C2bF4W8qazY7fHRf@cluster0-ts9ao.mongodb.net/test?retryWrites=true&w=majority",
-    secret: 'yoursecret'
-};
+if (process.env.NODE_ENV === "production") {
+    module.exports = {
+        mongoURI: "mongodb+srv://clinsmann:C2bF4W8qazY7fHRf@cluster0-ts9ao.mongodb.net/test?retryWrites=true&w=majority",
+        secret: 'yoursecret'
+    };
+} else {
+    module.exports = {
+        mongoURI: "mongodb://localhost/mevn_stack",
+        secret: 'yoursecret'
+    };
+}
